b11c2198211f52ab0b371cd9caf93420c3a28c8c
PrakarshKamal <kamalp@mcmaster.ca>
2023-09-12 17:04:25 -0400

    format and config changes

--- src/client/App.js
+++ src/client/App.js
@@ -1,15 +1,12 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import TabNavigator from './TabNavigator';
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import TabNavigator from "./TabNavigator";
 
 export default function App() {
   return (
     <NavigationContainer>
-    <TabNavigator />
-  </NavigationContainer>
-
+      <TabNavigator />
+    </NavigationContainer>
   );
 }
-
-
