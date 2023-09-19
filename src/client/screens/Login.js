239243df164fe481dc046a67456c0d091dbc8cfa
PrakarshKamal <kamalp@mcmaster.ca>
2023-09-19 03:36:51 -0400

    style changes to match figma

--- src/client/screens/Login.js
+++ src/client/screens/Login.js
@@ -1,13 +1,24 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, KeyboardAvoidingView } from 'react-native';
-import { FIREBASE_AUTH } from '../FirebaseConfig'; // Import the Firebase configuration
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Pressable,
+} from "react-native";
+import { FIREBASE_AUTH } from "../FirebaseConfig"; // Import the Firebase configuration
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { useNavigation } from "@react-navigation/native";
-import styles from '../styles/loginStyles';
+import styles from "../styles/loginStyles";
 
 function LoginScreen() {
-  const [email, setEmail] = useState(''); //useState<string>('') when changing to ts
-  const [password, setPassword] = useState(''); //useState<string>('') when changing to ts
+  const [email, setEmail] = useState(""); //useState<string>('') when changing to ts
+  const [password, setPassword] = useState(""); //useState<string>('') when changing to ts
   const [loading, setLoading] = useState(false);
   const auth = FIREBASE_AUTH;
   const navigation = useNavigation();
@@ -16,46 +27,51 @@ function LoginScreen() {
     setLoading(true);
     try {
       const response = await signInWithEmailAndPassword(auth, email, password);
-      console.log('User logged in:', response);
-      navigation.navigate('App');
+      console.log("User logged in:", response);
+      navigation.navigate("App");
     } catch (error) {
       console.log(error);
       //alert('Registration failed: ' + error.message);
     } finally {
-        setLoading(false);
+      setLoading(false);
     }
   };
-
-  
 
   // ... rest of the component ...
 
   return (
     <View style={styles.container}>
       <View style={styles.topHeadingContainer}>
-      <Text style={styles.titleText}>Login</Text> 
+        <Text style={styles.titleText}>Login</Text>
       </View>
       <View style={styles.middleContainer}>
-            <TextInput
-            placeholder="Email"
-            onChangeText={(text) => setEmail(text)}
-            value={email}
-            style={styles.input}
-            />
-            <TextInput
-            placeholder="Password"
-            onChangeText={(text) => setPassword(text)}
-            value={password}
-            secureTextEntry
-            style={styles.input}
-            />
-            <Button title="Login" onPress={signIn} />
-            </View>
+        <TextInput
+          placeholder="Email"
+          onChangeText={(text) => setEmail(text)}
+          value={email}
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Password"
+          onChangeText={(text) => setPassword(text)}
+          value={password}
+          secureTextEntry
+          style={styles.input}
+        />
 
-        <Text>OR </Text>
-        <Button title="Sign Up" onPress={() => navigation.navigate('Signup')} />
-        
-        </View>
+        <Pressable onPress={null}>
+          <Text style={styles.forgotPasswordText}>Forgot password?</Text>
+        </Pressable>
+
+        <Pressable style={styles.loginButton} onPress={signIn}>
+          <Text style={styles.loginButtonText}>Login</Text>
+        </Pressable>
+      </View>
+      <Text style={styles.noAccountText}>Don't have an account? </Text>
+      <Pressable onPress={() => navigation.navigate("Signup")}>
+        <Text style={styles.signUpButtonText}>Sign Up</Text>
+      </Pressable>
+    </View>
   );
 }
 
--- src/client/screens/Signup.js
+++ src/client/screens/Signup.js
@@ -1,15 +1,26 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, KeyboardAvoidingView } from 'react-native';
-import { FIREBASE_AUTH } from '../FirebaseConfig'; // Import the Firebase configuration
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Pressable,
+} from "react-native";
+import { FIREBASE_AUTH } from "../FirebaseConfig"; // Import the Firebase configuration
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { useNavigation } from "@react-navigation/native";
-import styles from '../styles/signupStyles';
+import styles from "../styles/signupStyles";
 
 function SignupScreen() {
-  const [email, setEmail] = useState(''); //useState<string>('') when changing to ts
-  const[firstName, setFirstName] = useState('');
-  const[lastName, setLastName] = useState('');
-  const [password, setPassword] = useState(''); //useState<string>('') when changing to ts
+  const [email, setEmail] = useState(""); //useState<string>('') when changing to ts
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [password, setPassword] = useState(""); //useState<string>('') when changing to ts
   const [loading, setLoading] = useState(false);
   const [isValidEmail, setIsValidEmail] = useState(true);
   const [inUseEmail, setEmailInUse] = useState(false);
@@ -20,74 +31,81 @@ function SignupScreen() {
   const signUp = async () => {
     setLoading(true);
     try {
-      const response = await createUserWithEmailAndPassword(auth, email, password);
-      console.log('User logged in:', response);
+      const response = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log("User logged in:", response);
       navigation.navigate("App");
     } catch (error) {
       console.log(error.code);
-      if (error.code === 'auth/invalid-email') {
+      if (error.code === "auth/invalid-email") {
         setIsValidEmail(false);
         console.log(error);
-
-      }
-      else if(error.code === 'auth/email-already-in-use'){
+      } else if (error.code === "auth/email-already-in-use") {
         setEmailInUse(true);
         console.log(error);
-      }
-      else if(error.code === 'auth/weak-password'){
+      } else if (error.code === "auth/weak-password") {
         setWeakPassword(true);
         console.log(error);
       }
     } finally {
-        setLoading(false);
+      setLoading(false);
     }
   };
-
-
 
   // ... rest of the component ...
 
   return (
     <View style={styles.container}>
       <View style={styles.topHeadingContainer}>
-      <Text style={styles.titleText}>Signup</Text> 
+        <Text style={styles.titleText}>Sign up</Text>
       </View>
       <View style={styles.middleContainer}>
-      <TextInput
-            placeholder="First Name"
-            onChangeText={(firstName) => setFirstName(firstName)}
-            value={firstName}
-            style={styles.input}
-            />
-            <TextInput
-            placeholder="Last Name"
-            onChangeText={(lastName) => setLastName(lastName)}
-            value={lastName}
-            style={styles.input}
-            />
-            {!isValidEmail&&<Text style={{color: 'red'}}>Email not valid </Text>}
-            {inUseEmail&& <Text style={{color: 'red'}}>Email already in use  </Text>}
-            <TextInput
-            placeholder="Email"
-            onChangeText={(email) => setEmail(email)}
-            value={email}
-            style={[styles.input, !isValidEmail && styles.inputError]}
-            />
-            {weakPassword && <Text style={{color: 'red'}}>Password is weak  </Text>}
-            <TextInput
-            placeholder="Password"
-            onChangeText={(password) => setPassword(password)}
-            value={password}
-            secureTextEntry
-            style={styles.input}
-            />
-            <Button title="Create Account" onPress={signUp} />
-            
-            </View>
-            <Text>OR, Already a USER?</Text>
-        <Button title="Log In" onPress={() => navigation.navigate('Login')} />
-        
-        </View>
+        <TextInput
+          placeholder="First Name"
+          onChangeText={(firstName) => setFirstName(firstName)}
+          value={firstName}
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Last Name"
+          onChangeText={(lastName) => setLastName(lastName)}
+          value={lastName}
+          style={styles.input}
+        />
+        {!isValidEmail && (
+          <Text style={{ color: "red" }}>Email not valid </Text>
+        )}
+        {inUseEmail && (
+          <Text style={{ color: "red" }}>Email already in use </Text>
+        )}
+        <TextInput
+          placeholder="Email"
+          onChangeText={(email) => setEmail(email)}
+          value={email}
+          style={[styles.input, !isValidEmail && styles.inputError]}
+        />
+        {weakPassword && (
+          <Text style={{ color: "red" }}>Password is weak </Text>
+        )}
+        <TextInput
+          placeholder="Password"
+          onChangeText={(password) => setPassword(password)}
+          value={password}
+          secureTextEntry
+          style={styles.input}
+        />
+        <Pressable style={styles.signUpButton} onPress={signUp}>
+          <Text style={styles.signUpButtonText}>Create Account</Text>
+        </Pressable>
+      </View>
+      <Text style={styles.alreadyAccountText}>Already have an account?</Text>
+      <Pressable onPress={() => navigation.navigate("Login")}>
+        <Text style={styles.logInButtonText}>Log In</Text>
+      </Pressable>
+    </View>
   );
 }
 
