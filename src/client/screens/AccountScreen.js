cdc7b60f21eb75eb011141cf34b591b81414121b
PrakarshKamal <kamalp@mcmaster.ca>
2023-09-21 17:48:44 -0400

    sign out button with styles

--- src/client/screens/AccountScreen.js
+++ src/client/screens/AccountScreen.js
@@ -1,18 +1,19 @@
-import { View, Text, Pressable } from 'react-native'
-import React from 'react'
-import { Button } from 'react-native'
-import useAuth from '../hooks/useAuth'
+import { View, Text, Pressable } from "react-native";
+import React from "react";
+import { Button } from "react-native";
+import useAuth from "../hooks/useAuth";
+import styles from "../styles/accountScreenStyles";
 
 const AccountScreen = () => {
-  const {signOutUser} = useAuth()
+  const { signOutUser } = useAuth();
   return (
     <View>
       <Text>AccountScreen</Text>
-      <Pressable onPress={signOutUser}>
-          <Text>Sign Out</Text>
-        </Pressable>
+      <Pressable style={styles.signOutButton} onPress={signOutUser}>
+        <Text style={styles.signOutButtonText}>Sign Out</Text>
+      </Pressable>
     </View>
-  )
-}
+  );
+};
 
-export default AccountScreen
+export default AccountScreen;
