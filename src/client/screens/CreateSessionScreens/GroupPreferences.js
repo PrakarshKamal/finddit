23d22ac9349afe1f7baa8eaa256bea6bc55c0c74
PrakarshKamal <kamalp@mcmaster.ca>
2023-11-02 02:59:34 -0400

    update styles for group preferences

--- src/client/screens/CreateSessionScreens/GroupPreferences.js
+++ src/client/screens/CreateSessionScreens/GroupPreferences.js
@@ -1,132 +1,229 @@
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Switch } from 'react-native';
-import React, { useState } from 'react'
-import styles from '../../styles/GroupPreferencesStyles';
-import Slider from '@react-native-community/slider';
-import useAuth from '../../hooks/useAuth';
-import {createNewGroup , getCardDataFromGroup} from '../../utils/api_function_calls/group_functions'
-import { useNavigation } from '@react-navigation/native';
-const GroupPreferences = ({route, navigation}) => {
-    const {groupName , groupIcon , groupMembers} = route.params
-    const [location, setLocation] = useState('');
-    const admin = useAuth()
-  const [radius, setRadius] = useState(5); // Default radius value
-  const [selectedPriceRange, setSelectedPriceRange] = useState(null);
-  const [openNow, setOpenNow] = useState(true);
-  const [groupDeadLine, setGroupDeadLine] = useState(24)
-  const[isActive, setIsActive] = useState(true)
+import {
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    StyleSheet,
+    Switch,
+} from "react-native";
+import React, { useState } from "react";
+import styles from "../../styles/GroupPreferencesStyles";
+import Slider from "@react-native-community/slider";
+import useAuth from "../../hooks/useAuth";
+import {
+    createNewGroup,
+    getCardDataFromGroup,
+} from "../../utils/api_function_calls/group_functions";
+import { useNavigation } from "@react-navigation/native";
+import { Divider } from "@rneui/themed";
+import { FontAwesome } from "@expo/vector-icons";
 
-  const handleRadiusChange = (value) => {
-    setRadius(value);
-  };
+const GroupPreferences = ({ route, navigation }) => {
+    const { groupName, groupIcon, groupMembers } = route.params;
+    const [location, setLocation] = useState("");
+    const admin = useAuth();
+    const [radius, setRadius] = useState(5); // Default radius value
+    const [selectedPriceRange, setSelectedPriceRange] = useState(null);
+    const [openNow, setOpenNow] = useState(true);
+    const [groupDeadLine, setGroupDeadLine] = useState(24);
+    const [isActive, setIsActive] = useState(true);
 
-  const handlePriceRangeSelect = (priceRange) => {
-    setSelectedPriceRange(priceRange);
-  };
-  const  handleGenerateButtonPressed =  async () => {
-    if(!radius || !selectedPriceRange || !openNow ){
-      alert("please enter valid fields")
-      return
-    }
-    const adminPreferences = {
-      latitude: 43.260838906356824 ,
-      longitude: -79.91288781166078 ,
-      radius: radius*10000 , 
-      keyword: "restaurant" ,
-      maxPrice: selectedPriceRange ,
-      minPrice: 1,
-      openNow : openNow
-    }
-    const groupMembersEmails = groupMembers.map(user => user.email);
-    const res = await createNewGroup(groupName,groupIcon,admin.user.email,groupMembersEmails,groupDeadLine,isActive,adminPreferences)
-    if(res.data){
-      const groupId = res.data
-      const cardData = await getCardDataFromGroup(groupId)
-      console.log(cardData.data)
-      const group = {groupName : groupName ,
-        groupId : groupId,
-        cardData : cardData.data,
-         groupIcon : groupIcon , 
-         groupMembers:groupMembers ,
-          groupAdmin :admin.user.email , 
-          groupDeadLine: groupDeadLine , 
-          isActive : isActive ,
-           adminPreferences : adminPreferences }
-        navigation.navigate("GroupCreated", group);
+    const handleRadiusChange = (value) => {
+        setRadius(value);
+    };
 
-    }else{
-      alert("Something went wrong")
-    }
-  }
-  return (
-    <View style={styles.container}>
-      <Text style={styles.groupName}>{groupName}</Text>
+    const handlePriceRangeSelect = (priceRange) => {
+        setSelectedPriceRange(priceRange);
+    };
+    const handleGenerateButtonPressed = async () => {
+        if (!radius || !selectedPriceRange || !openNow) {
+            alert("please enter valid fields");
+            return;
+        }
+        const adminPreferences = {
+            latitude: 43.260838906356824,
+            longitude: -79.91288781166078,
+            radius: radius * 10000,
+            keyword: "restaurant",
+            maxPrice: selectedPriceRange,
+            minPrice: 1,
+            openNow: openNow,
+        };
+        const groupMembersEmails = groupMembers.map((user) => user.email);
+        const res = await createNewGroup(
+            groupName,
+            groupIcon,
+            admin.user.email,
+            groupMembersEmails,
+            groupDeadLine,
+            isActive,
+            adminPreferences
+        );
+        if (res.data) {
+            const groupId = res.data;
+            const cardData = await getCardDataFromGroup(groupId);
+            console.log(cardData.data);
+            const group = {
+                groupName: groupName,
+                groupId: groupId,
+                cardData: cardData.data,
+                groupIcon: groupIcon,
+                groupMembers: groupMembers,
+                groupAdmin: admin.user.email,
+                groupDeadLine: groupDeadLine,
+                isActive: isActive,
+                adminPreferences: adminPreferences,
+            };
+            navigation.navigate("GroupCreated", group);
+        } else {
+            alert("Something went wrong");
+        }
+    };
+    return (
+        <View style={styles.container}>
+            <Text style={styles.groupName}>{groupName}</Text>
 
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Location</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Enter location"
-          value={location}
-          onChangeText={(text) => setLocation(text)}
-        />
-      </View>
+            <View style={styles.locationContainer}>
+                <Text style={styles.locationLabel}>LOCATION</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Enter location"
+                    value={location}
+                    onChangeText={(text) => setLocation(text)}
+                />
+            </View>
 
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Radius (miles): {radius}</Text>
-        <Slider
-          style={styles.slider}
-          minimumValue={1}
-          maximumValue={20}
-          thumbTintColor = {"#f27575"}
-          minimumTrackTintColor = {"#f27575"}
-          step={1}
-          value={radius}
-          onValueChange={handleRadiusChange}
-        />
-      </View>
+            <Divider width={1} />
 
-      <View >
-        <Text style={styles.label}>Price Range</Text>
-        <View style={styles.priceRangeContainer}>
-        <TouchableOpacity
-          style={[styles.priceRangeButton, selectedPriceRange === 1 && styles.selectedPriceRange]}
-          onPress={() => handlePriceRangeSelect(1)}
-        >
-          <Text style={styles.priceRangeText}>$</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={[styles.priceRangeButton, selectedPriceRange === 2 && styles.selectedPriceRange]}
-          onPress={() => handlePriceRangeSelect(2)}
-        >
-          <Text style={styles.priceRangeText}>$$</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={[styles.priceRangeButton, selectedPriceRange === 3 && styles.selectedPriceRange]}
-          onPress={() => handlePriceRangeSelect(3)}
-        >
-          <Text style={styles.priceRangeText}>$$$</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={[styles.priceRangeButton, selectedPriceRange === 4 && styles.selectedPriceRange]}
-          onPress={() => handlePriceRangeSelect(4)}
-        >
-          <Text style={styles.priceRangeText}>$$$$</Text>
-        </TouchableOpacity>
+            <View style={styles.radiusContainer}>
+                <Text style={styles.radiusLabel}>RADIUS (KM): {radius}</Text>
+                <Slider
+                    style={styles.slider}
+                    minimumValue={1}
+                    maximumValue={20}
+                    thumbTintColor={"#f27575"}
+                    minimumTrackTintColor={"#f27575"}
+                    step={1}
+                    value={radius}
+                    onValueChange={handleRadiusChange}
+                />
+            </View>
+
+            <Divider width={1} />
+
+            <View>
+                <Text style={styles.priceLabel}>PRICE RANGE</Text>
+                <View style={styles.priceRangeContainer}>
+                    <TouchableOpacity
+                        style={[
+                            styles.priceRangeButton,
+                            selectedPriceRange === 1 &&
+                                styles.selectedPriceRange,
+                        ]}
+                        onPress={() => handlePriceRangeSelect(1)}
+                    >
+                        <FontAwesome name="dollar" size={16} color="black" />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={[
+                            styles.priceRangeButton,
+                            selectedPriceRange === 2 &&
+                                styles.selectedPriceRange,
+                        ]}
+                        onPress={() => handlePriceRangeSelect(2)}
+                    >
+                        <View style={{ flexDirection: "row" }}>
+                            <FontAwesome
+                                name="dollar"
+                                size={16}
+                                color="black"
+                            />
+                            <FontAwesome
+                                name="dollar"
+                                size={16}
+                                color="black"
+                            />
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={[
+                            styles.priceRangeButton,
+                            selectedPriceRange === 3 &&
+                                styles.selectedPriceRange,
+                        ]}
+                        onPress={() => handlePriceRangeSelect(3)}
+                    >
+                        <View style={{ flexDirection: "row" }}>
+                            <FontAwesome
+                                name="dollar"
+                                size={16}
+                                color="black"
+                            />
+                            <FontAwesome
+                                name="dollar"
+                                size={16}
+                                color="black"
+                            />
+                            <FontAwesome
+                                name="dollar"
+                                size={16}
+                                color="black"
+                            />
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={[
+                            styles.priceRangeButton,
+                            selectedPriceRange === 4 &&
+                                styles.selectedPriceRange,
+                        ]}
+                        onPress={() => handlePriceRangeSelect(4)}
+                    >
+                        <View style={{ flexDirection: "row" }}>
+                            <FontAwesome
+                                name="dollar"
+                                size={16}
+                                color="black"
+                            />
+                            <FontAwesome
+                                name="dollar"
+                                size={16}
+                                color="black"
+                            />
+                            <FontAwesome
+                                name="dollar"
+                                size={16}
+                                color="black"
+                            />
+                            <FontAwesome
+                                name="dollar"
+                                size={16}
+                                color="black"
+                            />
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            </View>
+
+            <Divider width={1} />
+
+            <View style={styles.toggleButtonContainer}>
+                <Text style={styles.openNowSwitchLabel}>OPEN NOW</Text>
+                <Switch
+                    style={styles.openNowSwitch}
+                    trackColor={{ false: "", true: "#f27575" }}
+                    value={openNow}
+                    onValueChange={() => setOpenNow(!openNow)}
+                />
+            </View>
+            <TouchableOpacity
+                style={styles.nextButton}
+                onPress={handleGenerateButtonPressed}
+            >
+                <Text style={styles.nextButtonText}>Generate</Text>
+            </TouchableOpacity>
         </View>
-      </View>
-      <View style={styles.toggleButtonContainer}>
-      <Text style={styles.label}>Open Now:</Text>
-      <Switch
-        trackColor={{ false: '', true: '#f27575' }}
-        value={openNow}
-        onValueChange={() => setOpenNow(!openNow)}
-      />
-    </View>
-      <TouchableOpacity  style={styles.nextButton} onPress={handleGenerateButtonPressed}>
-            <Text style={styles.nextButtonText}>Generate</Text>
-      </TouchableOpacity>
-    </View>
-  );
-}
+    );
+};
 
-export default GroupPreferences
+export default GroupPreferences;
--- src/client/styles/GroupPreferencesStyles.js
+++ src/client/styles/GroupPreferencesStyles.js
@@ -1,71 +1,91 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      padding: 16,
+        flex: 1,
+        padding: 16,
     },
     groupName: {
-      fontSize: 24,
-      fontWeight: 'bold',
-      marginBottom: 20,
-      textAlign:'center'
+        fontSize: 24,
+        fontWeight: "bold",
+        marginBottom: 20,
+        textAlign: "center",
     },
-    inputContainer: {
-      marginBottom: 20,
+    locationContainer: {
+        marginTop: 20,
+        marginBottom: 10,
     },
-    label: {
-      fontSize: 18,
-      marginBottom: 8,
+    locationLabel: {
+        fontSize: 18,
+        marginBottom: 8,
+    },
+    radiusContainer: {
+        marginTop: 20,
+        marginBottom: 15,
+    },
+    radiusLabel: {
+        fontSize: 18,
+        marginBottom: 8,
     },
     input: {
-      fontSize: 16,
-      borderWidth: 1,
-      borderColor: '#ccc',
-      padding: 8,
+        fontSize: 16,
+        borderWidth: 1,
+        width: Dimensions.get("window").width - 50, // Take up full width
+        borderColor: "transparent",
     },
     slider: {
-      width: '100%',
+        width: "100%",
     },
     priceRangeContainer: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      marginBottom: 20,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginBottom: 20,
+    },
+    priceLabel: {
+        fontSize: 18,
+        marginBottom: 8,
+        marginTop: 20,
     },
     priceRangeButton: {
-      flex: 1,
-      borderWidth: 1,
-      borderColor: '#ccc',
-      padding: 10,
-      borderRadius: 50,
-      alignItems: 'center',
-      margin:2
+        flex: 1,
+        borderColor: "#rgba(242, 117, 117, 0.35)",
+        padding: 10,
+        borderWidth: 4,
+        borderRadius: 100,
+        alignItems: "center",
+        margin: 2,
     },
     selectedPriceRange: {
-      backgroundColor: "#f27575", 
+        backgroundColor: "#f27575",
     },
     priceRangeText: {
-      fontSize: 16,
+        fontSize: 16,
     },
     nextButton: {
-      position: 'absolute',
-      bottom: 20, 
-      right: 40, 
-      backgroundColor: "#f27575",
-      paddingVertical: 10,
-      paddingHorizontal: 20,
-      borderRadius: 5,
+        position: "absolute",
+        bottom: 20,
+        right: 40,
+        backgroundColor: "#f27575",
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 5,
     },
     nextButtonText: {
-      color: 'white',
-      fontSize: 16,
-      fontWeight: 'bold',
+        color: "white",
+        fontSize: 16,
+        fontWeight: "bold",
     },
     toggleButtonContainer: {
-      flexDirection: 'column',
+        flexDirection: "column",
     },
-    
-  });
+    openNowSwitchLabel: {
+        fontSize: 18,
+        marginTop: 16,
+    },
+    openNowSwitch: {
+        marginTop: 16,
+    },
+});
 
-  export default styles
+export default styles;
