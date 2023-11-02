8d0c37ff7e93b761fb2058cd2003074e7e6b57e0
PrakarshKamal <kamalp@mcmaster.ca>
2023-11-02 03:04:50 -0400

    add next button to add users screen

--- src/client/styles/AddUsersStyles.js
+++ src/client/styles/AddUsersStyles.js
@@ -2,31 +2,21 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      alignItems: 'center',
-      padding: 20,
+        flex: 1,
+        alignItems: "center",
+        padding: 20,
     },
-    
+
     nextButton: {
-      position: 'absolute',
-      bottom: 20, 
-      right: 40, 
-      backgroundColor: "#f27575",
-      paddingVertical: 10,
-      paddingHorizontal: 20,
-      borderRadius: 5,
+        position: "relative",
+        left: 110,
+        bottom: 40,
     },
-    nextButtonText: {
-      color: 'white',
-      fontSize: 16,
-      fontWeight: 'bold',
-    },
+
     iconGrid: {
-        alignItems: 'center',
-        justifyContent: 'center',
-      },
-    
-  });
-  
-  
-  export default styles 
+        alignItems: "center",
+        justifyContent: "center",
+    },
+});
+
+export default styles;
