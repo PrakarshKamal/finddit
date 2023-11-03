a52c09fbdf36657a04ecc032cfe365805ebe4e78
sandhk8 <sandhk8@mcmaster.ca>
2023-11-03 02:15:18 -0400

    added groupItem for acriveSessions

--- src/client/screens/ActiveSessions.js
+++ src/client/screens/ActiveSessions.js
@@ -1,35 +1,40 @@
 // ActiveSessions.js
 import { useNavigation } from '@react-navigation/native';
 import React, { useEffect, useState } from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, FlatList } from 'react-native';
 import {getActiveGroupsForUser} from '../utils/api_function_calls/group_functions'
+import GroupItem from '../components/GroupItem';
 
 const ActiveSessions = ({route}) => {
   const {email} = route.params
-  const [activeGroups , setActiveUsers] = useState(null);
+  const [activeGroups , setActiveGroups] = useState([]);
   const navigation = useNavigation();
   const fetchActiveGroups = async () => {
     try {
-     
       const response = await getActiveGroupsForUser(email)
       const data = await response.data;
       console.log('Active groups:', data);
-      setActiveUsers(data)
+      setActiveGroups(data)
     } catch (error) {
       console.error('Error fetching active groups:', error);
     }
   };
 
+  const renderActiveGroups = (item) => {
+    return <GroupItem group = {item.item}></GroupItem>
+  }
   useEffect(() => {
     fetchActiveGroups();
   }, []);
 
   return (
     <View>
-      <Text>Active Sessions</Text>
-      <Text>
-      {activeGroups}
-      </Text>
+      <Text style={{textAlign:'center'}}>Active Sessions</Text>
+      <FlatList
+      data={activeGroups}
+      renderItem={renderActiveGroups}>
+
+      </FlatList>
     </View>
   );
 };
