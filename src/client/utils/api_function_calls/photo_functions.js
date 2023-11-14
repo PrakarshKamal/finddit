c046fb6d24b5169733ace0d7afff9d56de6337c3
Japnit Singh <singj36@mcmaster.ca>
2023-11-13 21:54:47 -0500

    photos from ref without key

--- src/client/utils/api_function_calls/photo_functions.js
+++ src/client/utils/api_function_calls/photo_functions.js
@@ -3,7 +3,8 @@ const PHOTO_URL = "https://maps.googleapis.com/maps/api/place/photo";
 const API_KEY = process.env.GCP_KEY;
 
 async function getEmbedUrlFromPhotoRef(ref, maxwidth) {
-    const res = await axios.get(`${PHOTO_URL}?maxwidth=${maxwidth}&photo_reference=${ref}&key=AIzaSyA5sUNngPmqBGtRaYu2B9RZU6yshKW4StA`)
+    console.log(API_KEY)
+    const res = await axios.get(`${PHOTO_URL}?maxwidth=${maxwidth}&photo_reference=${ref}&key=`)
     return res.request.responseURL;
 }
 
