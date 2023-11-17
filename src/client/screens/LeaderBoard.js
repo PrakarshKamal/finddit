e195b521656738c45aeb1e13fd4d5d8e160cfd11
PrakarshKamal <kamalp@mcmaster.ca>
2023-11-17 00:45:01 -0500

    minor fix

--- src/client/screens/LeaderBoard.js
+++ src/client/screens/LeaderBoard.js
@@ -4,8 +4,16 @@ import React from "react";
 const LeaderBoard = () => {
     return (
         <View>
-            <Text style={{ textAlign: "center" }}>
-                LeaderBoard will be displayed here
+            <Text
+                style={{
+                    marginTop: 100,
+                    textAlign: "center",
+                    fontSize: 30,
+                    fontWeight: "bold",
+                    color: "gray",
+                }}
+            >
+                Leader board will be displayed here
             </Text>
         </View>
     );
