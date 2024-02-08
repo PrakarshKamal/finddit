d2e601dfdad1fa93fe20173109bc37b44c93846c
Japnit Singh <singj36@mcmaster.ca>
2024-02-07 19:42:51 -0500

    updated dto

--- src/backend/src/groups/dto/group-member-preferences.dto.ts
+++ src/backend/src/groups/dto/group-member-preferences.dto.ts
@@ -1,17 +1,9 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 export class GroupMemberPreferencesDto {
   @ApiProperty()
-  latitude: number;
-  @ApiProperty()
-  longitude: number;
-  @ApiProperty()
-  radius: number;
-  @ApiProperty()
   maxPrice: number;
   @ApiProperty()
   minPrice: number;
-  @ApiPropertyOptional()
-  willingToTravel: boolean;
-  @ApiPropertyOptional()
+  @ApiProperty()
   travelRadius: number;
 }
