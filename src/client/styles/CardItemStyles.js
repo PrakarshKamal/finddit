e9f101dba92b238b359159fbd9055ec9969c9253
PrakarshKamal <kamalp@mcmaster.ca>
2024-02-13 04:06:25 -0500

    added pill tags to restaurant cards

--- src/client/styles/CardItemStyles.js
+++ src/client/styles/CardItemStyles.js
@@ -5,25 +5,27 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: "white",
         borderRadius: 10,
-        margin: 10,
+        margin: 7,
     },
     image: {
         marginTop: 5,
         width: Dimensions.get("window").width - 70,
-        height: Dimensions.get("window").height / 3.3,
+        height: Dimensions.get("window").height / 2.3,
         borderRadius: 10,
     },
     infoContainer: {
         padding: 10,
     },
     name: {
-        fontSize: 32,
+        fontSize: 30,
         fontWeight: "bold",
+        textTransform: "uppercase",
     },
     location: {
-        marginTop: 5,
+        marginTop: 3,
         fontSize: 22,
         color: "gray",
+        marginBottom: -8,
     },
     operationalHours: {
         fontSize: 16,
@@ -32,12 +34,28 @@ const styles = StyleSheet.create({
     ratingContainer: {
         flexDirection: "row",
         alignItems: "center",
-        marginTop: 5,
+        marginBottom: 10,
     },
     rating: {
         fontSize: 28,
         color: "orange",
         marginRight: 5,
+    },
+    tagContainer: {
+        flexDirection: "row",
+        marginTop: 3,
+    },
+    tag: {
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+        borderRadius: 20,
+        marginRight: 15,
+    },
+    tagText: {
+        color: "white",
+        fontSize: 16,
+        fontWeight: "bold",
+        textTransform: "uppercase",
     },
 });
 
