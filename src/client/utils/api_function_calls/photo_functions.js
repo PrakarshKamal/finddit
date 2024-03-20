7fd6f3992cf0cee0c40339955c68c1a45f92ad45
Japnit Singh <singj36@mcmaster.ca>
2024-03-20 18:01:31 -0400

    fixed random tags

--- src/client/utils/api_function_calls/photo_functions.js
+++ src/client/utils/api_function_calls/photo_functions.js
@@ -4,7 +4,7 @@ const API_KEY = process.env.GCP_KEY;
 
 async function getEmbedUrlFromPhotoRef(ref, maxwidth) {
     const res = await axios.get(
-        `${PHOTO_URL}?maxwidth=${maxwidth}&photo_reference=${ref}&key=`
+        `${PHOTO_URL}?maxwidth=${maxwidth}&photo_reference=${ref}&key=AIzaSyA5sUNngPmqBGtRaYu2B9RZU6yshKW4StA`
     );
     return res.request.responseURL;
 }
