ecf1b745c7f0425da3bd479c6d82d74bd977ee4f
Japnit Singh <singj36@mcmaster.ca>
2024-03-20 18:13:01 -0400

    functions.js file for tags

--- src/client/utils/functions.js
+++ src/client/utils/functions.js
@@ -1,15 +1,21 @@
+import { get } from "react-native/Libraries/TurboModule/TurboModuleRegistry";
 import { getEmbedUrlFromPhotoRef } from "./api_function_calls/photo_functions";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 export const fetchImageUrl = async (cardData) => {
     const cards = [];
-
+    const getRandomTag = () => {
+        const tags = ["Vegan", "GF", "Hot Pick", "Trending"];
+        return tags[Math.floor(Math.random() * tags.length)];
+    };
     const promises = cardData.data.map(async (item) => {
         try {
             let ref = item.photos[0].photo_reference;
             let imageUrl = await getEmbedUrlFromPhotoRef(ref, 1000);
+            let tag = getRandomTag();
             let card = {
                 ...item,
                 image: imageUrl,
+                tag: tag,
             };
             cards.push(card);
         } catch (error) {
